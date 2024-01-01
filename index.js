a964c23fb5f1b7c331c5d1912e6aef64eb6f8940
Alex Lantz <aclantz321@gmail.com>
2024-01-01 16:42:18 -0500

    updated require, added movie and user arrays

--- index.js
+++ index.js
@@ -1,71 +1,81 @@
 const express = require("express"),
   morgan = require("morgan"),
+  bodyParser = require('body-parser'),
   app = express();
 
-//movie array for res.json (task-2.4) 
-let topMovies = [
+//movie array for res.json (task-2.4)
+let movies = [
   {
-    title: "The Lord of the Rings",
-    Director: "Peter Jackson",
+    name: 'The Lord of the Rings: Return of the King',
+    director: 'Peter Jackson',
+    year: '2003',
+    genre: 'Fantasy'
   },
   {
-    title: "The Birdcage",
-    Director: "Mike Nichols",
+    name: 'The Birdcage',
+    director: 'Mike Nichols',
+    year: '1996',
+    genre: 'Comedy'  
   },
   {
-    title: "The Shawshank Redemption",
-    director: "Frank Darabont",
+    name: 'Interstellar',
+    director: 'Christopher Nolan',
+    year: '2014',
+    genre: 'Sci-fi'
   },
   {
-    title: "Rogue One",
-    director: "Gareth Edwards",
+    name: 'Midsommar',
+    director: 'Ari Aster',
+    year: '2019',
+    genre: 'Horror'
   },
   {
-    title: "The Goonies",
-    director: "Richard Donner",
-  },
-  {
-    title: "The Devil Wears Prada",
-    director: "David Frankel",
-  },
-  {
-    title: "Interstellar",
-    director: "Christoper Nolan",
-  },
-  {
-    title: "Sunset Boulevard",
-    director: "Billy Wilder",
-  },
-  {
-    title: "Its a Wonderful Life",
-    director: "Frank Capra",
-  },
-  {
-    title: "Princess Mononoke",
-    director: "Hayao Miyazaki",
-  },
+    name: 'Pride and Prejudice',
+    director: 'Joe Wright',
+    year: '2005',
+    genre: 'Romance'
+  }
 ];
 
-//request log
-app.use(morgan('common'));
+let users = [
+  {
+    username: 'Sam12',
+    id:'',
+    favList: '',
+  },
+  {
+    username: 'Brock33',
+    id:'',
+    favList: '',
+  },
+  {
+    username: 'Dorthy45',
+    id:'',
+    favList: '',
+  }
+];
+
+//middleware call
+app.use(morgan("common"));
+app.use(bodyParser);
 
 //Static file route
 app.use(express.static("public"));
 
-//GET routes
-app.get("/movies", (req, res) => {
-  res.json(topMovies);
+//endpoint routes
+app.get('/movies', (req, res) => {
+  res.json(movies);
 });
-app.get("/", (req, res) => {
-  res.send("I love watching movies!");
-});
+app.get('/movies/:movie', (req, res) => {
+  
+})
 
 //error handler
 app.use((err, req, res, next) => {
-    console.log(err.stack);
-  });
+  console.log(err.stack);
+});
 
 //listen for requests
 app.listen(8080, () => {
-    console.log('Your app is listening on port 8080');
+  console.log("Your app is listening on port 8080");
 });
